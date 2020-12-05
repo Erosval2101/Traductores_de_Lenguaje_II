5dadbb69f8914c50be6bdc66588771bb30da6203
Erosval2101 <70926870+Erosval2101@users.noreply.github.com>
2020-12-04 22:23:40 -0600

    Tarea 2.
    Subiendo archivos de la Tarea 2 (Analizador Léxico).

---
+++ Tarea 2/analizadorLexico/AnalizadorLexico.cs
@@ -0,0 +1,533 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace analizadorLexico
+{
+
+    class AnalizadorLexico
+    {
+        //Símbolos
+        public const int ERROR = -1;
+        public const int INICIAL = 0;
+        public const int IDENTIFICADOR = 1;
+        public const int ENTERO = 2;
+        public const int REAL = 3;
+        public const int CADENA = 4;
+        public const int TIPO =  5;
+        public const int OPSUMA = 6;
+        public const int OPMUL = 7;
+        public const int OPRELAC = 8;
+        public const int OPOR = 9;
+        public const int OPAND = 10;
+        public const int OPNOT = 11;
+        public const int OPIGUALDAD = 12;
+        public const int PUNTOCOMA = 13;
+        public const int COMA = 14;
+        public const int ABREPARENTESIS= 15;
+        public const int CIERRAPARENTESIS = 16;
+        public const int ABRELLAVE = 17;
+        public const int CIERRALLAVE = 18;
+        public const int IGUAL = 19;
+        public const int IF = 20;
+        public const int WHILE = 21;
+        public const int RETURN = 22;
+        public const int ELSE = 23;
+        public const int SIGNOPESOS = 24;
+
+        public String[] tipoDatos = new String[] { "int", "float", "void" };
+        public String[] reservadas = new String[] { "if", "while", "return", "else" };
+
+
+        List<String> listaErrores;
+        List<String> listaTokens;
+
+        public AnalizadorLexico()
+        {
+            listaErrores = new List<String>();
+            listaTokens = new List<String>();
+        }
+
+        public void agregaToken(String lexema, int tipo)
+        {
+            //Añade nuevo token
+            String nuevoToken;
+            switch (tipo)
+            {
+                case IDENTIFICADOR:
+                    nuevoToken = lexema + " es un identificador" + "\n";
+                    listaTokens.Add(nuevoToken);
+                    break;
+                case ENTERO:
+                    nuevoToken = lexema + " es un entero" + "\n";
+                    listaTokens.Add(nuevoToken);
+                    break;
+                case REAL:
+                    nuevoToken = lexema + " es un número real" + "\n";
+                    listaTokens.Add(nuevoToken);
+                    break;
+                case CADENA:
+                    nuevoToken = lexema + " es una cadena" + "\n";
+                    listaTokens.Add(nuevoToken);
+                    break;
+                case TIPO:
+                    nuevoToken = lexema + " es un tipo de dato" + "\n";
+                    listaTokens.Add(nuevoToken);
+                    break;
+                case OPSUMA:
+                    if (lexema == "+")
+                    {
+                        nuevoToken = lexema + " es un operador de suma" + "\n";
+                        listaTokens.Add(nuevoToken);
+                    }
+                    else if (lexema == "-")
+                    {
+                        nuevoToken = lexema + " es un operador de resta" + "\n";
+                        listaTokens.Add(nuevoToken);
+                    }
+                    break;
+                case OPMUL:
+                    if (lexema == "*")
+                    {
+                        nuevoToken = lexema + " es un operador de multiplicación" + "\n";
+                        listaTokens.Add(nuevoToken);
+                    }
+                    else if (lexema == "/")
+                    {
+                        nuevoToken = lexema + " es un operador de división" + "\n";
+                        listaTokens.Add(nuevoToken);
+                    }
+                    break;
+                case OPRELAC:
+                    nuevoToken = lexema + " es un operador relacional" + "\n";
+                    listaTokens.Add(nuevoToken);
+                    break;
+                case OPOR:
+                    nuevoToken = lexema + " es un operador " + "OR" + "\n";
+                    listaTokens.Add(nuevoToken);
+                    break;
+                case OPAND:
+                    nuevoToken = lexema + " es un operador AND" + "\n";
+                    listaTokens.Add(nuevoToken);
+                    break;
+                case OPNOT:
+                    nuevoToken = lexema + " es un operador de negación" + "\n";
+                    listaTokens.Add(nuevoToken);
+                    break;
+                case OPIGUALDAD:
+                    nuevoToken = lexema + " es un operador de igualdad" + "\n";
+                    listaTokens.Add(nuevoToken);
+                    break;
+                case PUNTOCOMA:
+                    nuevoToken = lexema + " es un punto y coma (;)" + "\n";
+                    listaTokens.Add(nuevoToken);
+                    break;
+                case COMA:
+                    nuevoToken = lexema + " es una coma (,)" + "\n";
+                    listaTokens.Add(nuevoToken);
+                    break;
+                case ABREPARENTESIS:
+                    nuevoToken = lexema + " abre paréntesis" + "\n";
+                    listaTokens.Add(nuevoToken);
+                    break;
+                case CIERRAPARENTESIS:
+                    nuevoToken = lexema + " cierra paréntesis" + "\n";
+                    listaTokens.Add(nuevoToken);
+                    break;
+                case ABRELLAVE:
+                    nuevoToken = lexema + " abre llave" + "\n";
+                    listaTokens.Add(nuevoToken);
+                    break;
+                case CIERRALLAVE:
+                    nuevoToken = lexema + " cierra llave" + "\n";
+                    listaTokens.Add(nuevoToken);
+                    break;
+                case IGUAL:
+                    nuevoToken = lexema + " es un operador de igual (=)" + "\n";
+                    listaTokens.Add(nuevoToken);
+                    break;
+                case IF:
+                case WHILE:
+                case RETURN:
+                case ELSE:
+                    nuevoToken = lexema + " es una palabra reservada" + "\n";
+                    listaTokens.Add(nuevoToken);
+                    break;
+                case SIGNOPESOS:
+                    nuevoToken = lexema + " es el signo $" + "\n";
+                    listaTokens.Add(nuevoToken);
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        public void agregaError(String lexema)
+        {
+            //Añade un error
+            String nuevoError;
+            nuevoError = lexema + " es un carácter no admitido" + "\n";
+            listaErrores.Add(nuevoError);
+        }
+
+
+        public void Analizador(String texto)
+        {
+            int estado = 0; //Identifica el estado actual del analizador
+            String lexema = "";
+            Char c;
+            bool hayPunto = false; //Bandera para verificar si la expresión es un número y si tiene un punto
+            texto = texto + " ";
+
+            //Inicia el automata del analizador
+            for (int i = 0; i < texto.Length; i++)
+            {
+                c = texto[i];
+                switch (estado)
+                {
+                    case INICIAL:
+                        if (Char.IsLetter(c) || c == '_')
+                        { //Verifica si es letra o empieza con un "_"
+                            estado = IDENTIFICADOR;
+                            lexema += c;
+                        }
+                        else if (Char.IsDigit(c))
+                        { //Verifica si es digito
+                            estado = ENTERO;
+                            lexema += c;
+                        }
+                        else if (c == '"')
+                        {
+                            estado = CADENA;
+                            lexema += c;
+                        } 
+                        else if(c == '+' || c == '-')
+                        {
+                            lexema += c;
+
+                            agregaToken(lexema, OPSUMA);
+                            estado = INICIAL;
+                            lexema = "";
+                        }
+                        else if (c == '*' || c == '/')
+                        {
+                            lexema += c;
+
+                            agregaToken(lexema, OPMUL);
+                            estado = INICIAL;
+                            lexema = "";
+                        }
+                        else if (c == '<' || c == '>')
+                        {
+                            estado = OPRELAC;
+                            lexema += c;
+                        }
+                        else if (c == '|')
+                        {
+                            estado = OPOR;
+                            lexema += c;
+                        }
+                        else if (c == '&')
+                        {
+                            estado = OPAND;
+                            lexema += c;
+                        }
+                        else if (c == '='|| c == '!')
+                        {
+                            if (texto[i + 1] != '=')
+                            {
+                                if (c == '=')
+                                {
+                                    lexema += c;
+
+                                    agregaToken(lexema, IGUAL);
+                                    lexema = "";
+                                    estado = INICIAL;
+                                }
+                                else if (c == '!')
+                                {
+                                    lexema += c;
+
+                                    agregaToken(lexema, OPNOT);
+                                    estado = INICIAL;
+                                    lexema = "";
+                                }
+                            }
+                            else
+                            {
+                                estado = OPIGUALDAD;
+                                lexema += c;
+                            }
+                        }
+                        else if (c == ';')
+                        {
+                            lexema += c;
+
+                            agregaToken(lexema, PUNTOCOMA);
+                            estado = INICIAL;
+                            lexema = "";
+                        }
+                        else if (c == ',')
+                        {
+                            lexema += c;
+
+                            agregaToken(lexema, COMA);
+                            estado = INICIAL;
+                            lexema = "";
+                        }
+                        else if (c == '(')
+                        {
+                            lexema += c;
+
+                            agregaToken(lexema, ABREPARENTESIS);
+                            estado = INICIAL;
+                            lexema = "";
+                        }
+                        else if (c == ')')
+                        {
+                            lexema += c;
+
+                            agregaToken(lexema, CIERRAPARENTESIS);
+                            estado = INICIAL;
+                            lexema = "";
+                        }
+                        else if (c == '{')
+                        {
+                            lexema += c;
+
+                            agregaToken(lexema, ABRELLAVE);
+                            estado = INICIAL;
+                            lexema = "";
+                        }
+                        else if (c == '}')
+                        {
+                            lexema += c;
+
+                            agregaToken(lexema, CIERRALLAVE);
+                            estado = INICIAL;
+                            lexema = "";
+                        }
+                        else if (c == '$')
+                        {
+                            lexema += c;
+
+                            agregaToken(lexema, SIGNOPESOS);
+                            estado = INICIAL;
+                            lexema = "";
+                        }
+                        break;
+                    case IDENTIFICADOR:
+                        if (Char.IsLetterOrDigit(c) || c == '_')
+                        {
+                            estado = IDENTIFICADOR;
+                            lexema += c;
+                        }
+                        else
+                        {
+                            if (esTipoDato(lexema))
+                            {
+                                agregaToken(lexema, TIPO);
+                                estado = INICIAL;
+                                lexema = "";
+                            }
+                            else if (esReservada(lexema))
+                            {
+                                agregaToken(lexema, tipoReservada(lexema));
+                                estado = INICIAL;
+                                lexema = "";
+                            }
+                            else
+                            {
+                                agregaToken(lexema, IDENTIFICADOR);
+                                estado = INICIAL;
+                                lexema = "";
+                            }
+                        }
+                        break;
+                    case ENTERO:
+                        if (Char.IsDigit(c))
+                        {
+                            estado = ENTERO;
+                            lexema += c;
+                        }
+                        else if (c == '.')
+                        {
+                            if (hayPunto == false)
+                            {
+                                estado = REAL;
+                                lexema += c;
+                                hayPunto = true;
+
+                            }
+                            else
+                            {
+                                lexema += c;
+                                agregaError(lexema);
+
+                                estado = INICIAL;
+                                lexema = "";
+                            }
+                        }
+                        else
+                        {
+                            agregaToken(lexema, ENTERO);
+                            estado = INICIAL;
+                            lexema = "";
+                            i--;
+                        }
+                        break;
+                    case REAL:
+                        if (Char.IsDigit(c))
+                        {
+                            estado = REAL;
+                            lexema += c;
+                        }
+                        else if (c == '.')
+                        {
+                            if (hayPunto)
+                            {
+                                lexema += c;
+                                agregaError(lexema);
+
+                                estado = INICIAL;
+                                lexema = "";
+                            }
+                        }
+                        else
+                        {
+                            agregaToken(lexema, REAL);
+                            estado = INICIAL;
+                            lexema = "";
+                        }
+                        break;
+                    case CADENA:
+                        if (c == '"')
+                        {
+                            lexema += c;
+                            agregaToken(lexema, CADENA);
+                            estado = INICIAL;
+                            lexema = "";
+                        }
+                        else {
+                            estado = CADENA;
+                            lexema += c;
+                        }
+                        break;
+                    case OPRELAC:
+                        if (c == '=')
+                        {
+                            lexema += c;
+
+                            agregaToken(lexema, OPRELAC);
+                            estado = INICIAL;
+                            lexema = "";
+                        }
+                        else
+                        {
+                            agregaToken(lexema, OPRELAC);
+                            estado = INICIAL;
+                            lexema = "";
+                        }
+                        break;
+                    case OPOR:
+                        if(c == '|')
+                        {
+                            lexema += c;
+
+                            agregaToken(lexema, OPOR);
+                            estado = INICIAL;
+                            lexema = "";
+                        }
+                        else
+                        {
+                            agregaToken(lexema, OPOR);
+                            estado = INICIAL;
+                            lexema = "";
+                        }
+                        break;
+                    case OPAND:
+                        if (c == '&')
+                        {
+                            lexema += c;
+
+                            agregaToken(lexema, OPAND);
+                            estado = INICIAL;
+                            lexema = "";
+                        }
+                        else
+                        {
+                            agregaToken(lexema, OPAND);
+                            estado = INICIAL;
+                            lexema = "";
+                        }
+                        break;
+                    case OPIGUALDAD:
+                        lexema += c;
+
+                        agregaToken(lexema, OPIGUALDAD);
+                        estado = INICIAL;
+                        lexema = "";
+                        break;
+                    default:
+                        break;
+                }
+            }
+            //Termina el automata
+        }
+
+        public Boolean esTipoDato(String lexema)
+        {
+            for (int i = 0; i < tipoDatos.Length; i++)
+            {
+                if (tipoDatos[i].Equals(lexema))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+
+        public Boolean esReservada(String lexema)
+        {
+            for (int i = 0; i < reservadas.Length; i++)
+            {
+                if (reservadas[i].Equals(lexema))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public int tipoReservada(String lexema)
+        {
+            switch (lexema)
+            {
+                case "if":
+                    return IF;
+                case "while":
+                    return WHILE;
+                case "return":
+                    return RETURN;
+                case "else":
+                    return ELSE;
+                default:
+                    return ERROR;
+            }
+        }
+
+        public String dameListaToken()
+        {
+            String lista = String.Join(Environment.NewLine, listaTokens.ToArray());
+            return lista;
+
+        }
+
+        public String dameListaErrores()
+        {
+            String lista = String.Join(Environment.NewLine, listaErrores.ToArray());
+            return lista;
+        }
+
+    }
+}
---
+++ Tarea 2/analizadorLexico/Principal.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace analizadorLexico
+{
+    partial class Principal
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtExpresion = new System.Windows.Forms.TextBox();
+            this.lblIngresar = new System.Windows.Forms.Label();
+            this.btnAnalizar = new System.Windows.Forms.Button();
+            this.txtResultado = new System.Windows.Forms.TextBox();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.txtErrores = new System.Windows.Forms.TextBox();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.lblErrores = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtExpresion
+            // 
+            this.txtExpresion.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtExpresion.Location = new System.Drawing.Point(12, 44);
+            this.txtExpresion.Name = "txtExpresion";
+            this.txtExpresion.Size = new System.Drawing.Size(488, 32);
+            this.txtExpresion.TabIndex = 0;
+            this.txtExpresion.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtExpresion_KeyPress);
+            // 
+            // lblIngresar
+            // 
+            this.lblIngresar.AutoSize = true;
+            this.lblIngresar.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblIngresar.Location = new System.Drawing.Point(12, 17);
+            this.lblIngresar.Name = "lblIngresar";
+            this.lblIngresar.Size = new System.Drawing.Size(177, 24);
+            this.lblIngresar.TabIndex = 1;
+            this.lblIngresar.Text = "Ingrese expresión";
+            // 
+            // btnAnalizar
+            // 
+            this.btnAnalizar.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAnalizar.Location = new System.Drawing.Point(506, 43);
+            this.btnAnalizar.Name = "btnAnalizar";
+            this.btnAnalizar.Size = new System.Drawing.Size(125, 32);
+            this.btnAnalizar.TabIndex = 2;
+            this.btnAnalizar.Text = "Analizar";
+            this.btnAnalizar.UseVisualStyleBackColor = true;
+            this.btnAnalizar.Click += new System.EventHandler(this.btnAnalizar_Click);
+            // 
+            // txtResultado
+            // 
+            this.txtResultado.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtResultado.Location = new System.Drawing.Point(12, 117);
+            this.txtResultado.Multiline = true;
+            this.txtResultado.Name = "txtResultado";
+            this.txtResultado.ReadOnly = true;
+            this.txtResultado.Size = new System.Drawing.Size(292, 298);
+            this.txtResultado.TabIndex = 3;
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLimpiar.Location = new System.Drawing.Point(203, 422);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(239, 32);
+            this.btnLimpiar.TabIndex = 4;
+            this.btnLimpiar.Text = "Limpiar";
+            this.btnLimpiar.UseVisualStyleBackColor = true;
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
+            // 
+            // txtErrores
+            // 
+            this.txtErrores.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtErrores.Location = new System.Drawing.Point(338, 117);
+            this.txtErrores.Multiline = true;
+            this.txtErrores.Name = "txtErrores";
+            this.txtErrores.ReadOnly = true;
+            this.txtErrores.Size = new System.Drawing.Size(293, 298);
+            this.txtErrores.TabIndex = 6;
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResultado.Location = new System.Drawing.Point(12, 92);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(96, 22);
+            this.lblResultado.TabIndex = 7;
+            this.lblResultado.Text = "Resultado";
+            // 
+            // lblErrores
+            // 
+            this.lblErrores.AutoSize = true;
+            this.lblErrores.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblErrores.Location = new System.Drawing.Point(334, 92);
+            this.lblErrores.Name = "lblErrores";
+            this.lblErrores.Size = new System.Drawing.Size(73, 22);
+            this.lblErrores.TabIndex = 8;
+            this.lblErrores.Text = "Errores";
+            // 
+            // Principal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(643, 466);
+            this.Controls.Add(this.lblErrores);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.txtErrores);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.txtResultado);
+            this.Controls.Add(this.btnAnalizar);
+            this.Controls.Add(this.lblIngresar);
+            this.Controls.Add(this.txtExpresion);
+            this.Name = "Principal";
+            this.Text = "Analizador Lexico";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtExpresion;
+        private System.Windows.Forms.Label lblIngresar;
+        private System.Windows.Forms.Button btnAnalizar;
+        private System.Windows.Forms.TextBox txtResultado;
+        private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.TextBox txtErrores;
+        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.Label lblErrores;
+    }
+}
+
